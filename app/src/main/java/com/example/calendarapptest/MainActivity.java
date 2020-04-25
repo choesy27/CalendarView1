e86f4546fee75b3cac2e500fa49cec4e03d64b11
choesy27 <choesy@suwon.ac.kr>
2020-04-25 19:29:01 +0900

    Initial commit

---
+++ app/src/main/java/com/example/calendarapptest/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.calendarapptest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
